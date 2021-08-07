da4aeab9dbac8376d7ac278bd6850bb60a9ff3a0
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-08 00:18:58 +0900

    fortated

--- src/libs/lambda.ts
+++ src/libs/lambda.ts
@@ -1,6 +1,8 @@
 import middy from '@middy/core'
 import middyJsonBodyParser from '@middy/http-json-body-parser'
 
-export const middyfy = (handler: Parameters<typeof middy>[0]): middy.Middy<unknown, unknown> => {
+export const middyfy = (
+  handler: Parameters<typeof middy>[0]
+): middy.Middy<unknown, unknown> => {
   return middy(handler).use(middyJsonBodyParser())
 }
