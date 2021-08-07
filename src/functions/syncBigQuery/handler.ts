5f1bc7ebe53df9e85003dc096b453ac47838b3ea
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-08 00:18:16 +0900

    wip: syncBigQuery

--- src/functions/syncBigQuery/handler.ts
+++ src/functions/syncBigQuery/handler.ts
@@ -1,7 +1,26 @@
 import { middyfy } from '@libs/lambda'
+import * as Shopify from 'shopify-api-node'
+
+const shopify = new Shopify({
+  shopName: process.env.SHOPIFY_SHOP_NAME,
+  apiKey: process.env.SHOPIFY_API_KEY,
+  password: process.env.SHOPIFY_API_SECRET_KEY
+})
+
+const query = `{
+  customers(first: 5) {
+    edges {
+      node {
+        displayName
+        totalSpent
+      }
+    }
+  }
+}`
 
 const orders = async () => {
-  console.log('test')
+  const c = await shopify.graphql(query)
+  console.log(c)
 }
 
 export const main = middyfy(orders)
--- src/functions/syncBigQuery/index.ts
+++ src/functions/syncBigQuery/index.ts
@@ -1,6 +1,12 @@
 import { handlerPath } from '@libs/handlerResolver'
 import { AWS } from '@serverless/typescript'
 
+const environment = {
+  SHOPIFY_SHOP_NAME: '${env:SHOPIFY_SHOP_NAME}',
+  SHOPIFY_API_KEY: '${env:SHOPIFY_API_KEY}',
+  SHOPIFY_API_SECRET_KEY: '${env:SHOPIFY_API_SECRET_KEY}'
+}
+
 const func: AWS['functions'][string] = {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
@@ -9,7 +15,10 @@ const func: AWS['functions'][string] = {
         rate: '2 hours'
       }
     }
-  ]
+  ],
+  environment: {
+    ...environment
+  }
 }
 
 export default func
