a3ece8bf4ccdf3e8b4d91e15bc61cca0fa8cedbc
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2021-08-29 15:27:41 +0900

    WIP: facebook ad management

--- src/functions/syncBigQuery/handler.ts
+++ src/functions/syncBigQuery/handler.ts
@@ -311,6 +311,8 @@ type AdSetInsights = {
     spend: string
     reach: string
     clicks: string
+    actions?: { action_type: string; value: string }[]
+    action_values?: { action_type: string; value: string }[]
     date_start: string
     date_stop: string
   }[]
@@ -354,6 +356,8 @@ type AdReportRecord = {
   spend: number
   reach: number
   clicks: number
+  conversions: number
+  return: number
   date: string
 }
 
@@ -361,7 +365,7 @@ const getAdReportRecords = async (
   inspectDate: string
 ): Promise<AdReportRecord[]> => {
   const records: AdReportRecord[] = []
-  let next = `https://graph.facebook.com/v11.0/${process.env.FACEBOOK_BUSINESS_ACCOUNT_ID}?fields=owned_ad_accounts.limit(5){name,adsets.limit(20){name,insights.time_range({since:'${inspectDate}',until:'${inspectDate}'}){impressions,spend,reach,clicks}}}&access_token=${process.env.FACEBOOK_GRAPH_API_TOKEN}`
+  let next = `https://graph.facebook.com/v11.0/${process.env.FACEBOOK_BUSINESS_ACCOUNT_ID}?fields=owned_ad_accounts.limit(5){name,adsets.limit(20){name,insights.time_range({since:'${inspectDate}',until:'${inspectDate}'}){impressions,spend,reach,clicks,action_values,actions}}}&access_token=${process.env.FACEBOOK_GRAPH_API_TOKEN}`
   while (next) {
     const res = await fetch(next).then(
       (res) => res.json() as Promise<Res | AdAccount>
@@ -376,7 +380,15 @@ const getAdReportRecords = async (
     adAccount.forEach(({ id: adAccountId, name: adAccountName, adsets }) => {
       adsets.data.forEach(({ id: adSetId, name: adSetName, insights }) => {
         insights?.data.forEach(
-          ({ impressions, spend, reach, clicks, date_start: date }) => {
+          ({
+            impressions,
+            spend,
+            reach,
+            clicks,
+            actions,
+            action_values,
+            date_start: date
+          }) => {
             records.push({
               adAccountId,
               adAccountName,
@@ -386,6 +398,16 @@ const getAdReportRecords = async (
               spend: Number(spend),
               reach: Number(reach),
               clicks: Number(clicks),
+              conversions: Number(
+                actions?.find(
+                  ({ action_type }) => action_type === 'omni_purchase'
+                )?.value || 0
+              ),
+              return: Number(
+                action_values?.find(
+                  ({ action_type }) => action_type === 'omni_purchase'
+                )?.value || 0
+              ),
               date
             })
           }
