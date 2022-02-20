7e114d6f4faeb95dac8eba47c7a203ecfed3b07f
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-02-20 11:16:40 +0900

    refactor

--- src/functions/syncBigQuery/shopify.ts
+++ src/functions/syncBigQuery/shopify.ts
@@ -577,7 +577,7 @@ const convertSchedule = (value: string): string => {
       ? 'early'
       : day === '20'
       ? 'middle'
-      : ['28', '29', '31', '30'].includes(day)
+      : ['28', '29', '30', '31'].includes(day)
       ? 'late'
       : null
   if (term === null) throw new Error(`parse error: ${value}`)
